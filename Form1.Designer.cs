86be7ec479b415c1273c2219a401dabfae85f708
Oluwasegun Atolagbe <58482668+AtVict0r@users.noreply.github.com>
2022-12-19 14:01:51 -0500

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,219 @@
+﻿namespace CountdownTimer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.onesSecondCounter = new System.Windows.Forms.NumericUpDown();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tensSecondCounter = new System.Windows.Forms.NumericUpDown();
+            this.onesMinuteCounter = new System.Windows.Forms.NumericUpDown();
+            this.tensMinuteCounter = new System.Windows.Forms.NumericUpDown();
+            this.controlButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.onesSecondCounter)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tensSecondCounter)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.onesMinuteCounter)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tensMinuteCounter)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Consolas", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(28, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(239, 32);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Countdown Timer";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Open Sans", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(29, 57);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(81, 28);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Minute";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Open Sans", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(169, 57);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(85, 28);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Second";
+            // 
+            // onesSecondCounter
+            // 
+            this.onesSecondCounter.Font = new System.Drawing.Font("Open Sans Extrabold", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.onesSecondCounter.Location = new System.Drawing.Point(220, 98);
+            this.onesSecondCounter.Maximum = new decimal(new int[] {
+            9,
+            0,
+            0,
+            0});
+            this.onesSecondCounter.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            -2147483648});
+            this.onesSecondCounter.Name = "onesSecondCounter";
+            this.onesSecondCounter.Size = new System.Drawing.Size(47, 51);
+            this.onesSecondCounter.TabIndex = 3;
+            this.onesSecondCounter.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Lithograph", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(129, 102);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(27, 39);
+            this.label4.TabIndex = 4;
+            this.label4.Text = ":";
+            // 
+            // tensSecondCounter
+            // 
+            this.tensSecondCounter.Font = new System.Drawing.Font("Open Sans Extrabold", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tensSecondCounter.Location = new System.Drawing.Point(162, 98);
+            this.tensSecondCounter.Maximum = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            this.tensSecondCounter.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            -2147483648});
+            this.tensSecondCounter.Name = "tensSecondCounter";
+            this.tensSecondCounter.Size = new System.Drawing.Size(47, 51);
+            this.tensSecondCounter.TabIndex = 5;
+            this.tensSecondCounter.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // onesMinuteCounter
+            // 
+            this.onesMinuteCounter.Font = new System.Drawing.Font("Open Sans Extrabold", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.onesMinuteCounter.Location = new System.Drawing.Point(76, 98);
+            this.onesMinuteCounter.Maximum = new decimal(new int[] {
+            9,
+            0,
+            0,
+            0});
+            this.onesMinuteCounter.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            -2147483648});
+            this.onesMinuteCounter.Name = "onesMinuteCounter";
+            this.onesMinuteCounter.Size = new System.Drawing.Size(47, 51);
+            this.onesMinuteCounter.TabIndex = 6;
+            this.onesMinuteCounter.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // tensMinuteCounter
+            // 
+            this.tensMinuteCounter.Font = new System.Drawing.Font("Open Sans Extrabold", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tensMinuteCounter.Location = new System.Drawing.Point(23, 98);
+            this.tensMinuteCounter.Maximum = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            this.tensMinuteCounter.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            -2147483648});
+            this.tensMinuteCounter.Name = "tensMinuteCounter";
+            this.tensMinuteCounter.Size = new System.Drawing.Size(47, 51);
+            this.tensMinuteCounter.TabIndex = 7;
+            this.tensMinuteCounter.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // controlButton
+            // 
+            this.controlButton.Font = new System.Drawing.Font("Consolas", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.controlButton.Location = new System.Drawing.Point(76, 183);
+            this.controlButton.Name = "controlButton";
+            this.controlButton.Size = new System.Drawing.Size(121, 50);
+            this.controlButton.TabIndex = 8;
+            this.controlButton.Text = "Start";
+            this.controlButton.UseVisualStyleBackColor = true;
+            this.controlButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(307, 280);
+            this.Controls.Add(this.controlButton);
+            this.Controls.Add(this.tensMinuteCounter);
+            this.Controls.Add(this.onesMinuteCounter);
+            this.Controls.Add(this.tensSecondCounter);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.onesSecondCounter);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(323, 319);
+            this.MinimumSize = new System.Drawing.Size(323, 319);
+            this.Name = "Form1";
+            this.Text = "Countdown Timer";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.onesSecondCounter)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tensSecondCounter)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.onesMinuteCounter)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tensMinuteCounter)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown onesSecondCounter;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.NumericUpDown tensSecondCounter;
+        private System.Windows.Forms.NumericUpDown onesMinuteCounter;
+        private System.Windows.Forms.NumericUpDown tensMinuteCounter;
+        private System.Windows.Forms.Button controlButton;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows.Forms;
+
+namespace CountdownTimer
+{
+    public partial class Form1 : Form
+    {
+        System.Timers.Timer time;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            time = new System.Timers.Timer();
+            time.Interval = 1000; // 1 Second
+            time.Elapsed += OnTimeEvent;
+        }
+
+        private void OnTimeEvent(object sender, ElapsedEventArgs e)
+        {
+            Invoke(new Action(() =>
+            {
+                // set ones second counter value
+                onesSecondCounter.Value--;
+                if (tensMinuteCounter.Value == 0 && onesMinuteCounter.Value == 0 && tensSecondCounter.Value == 0 && onesSecondCounter.Value == -1)
+                {
+                    onesSecondCounter.Value = 0;
+                    time.Stop();
+                    controlButton.Text = "Start";
+
+                    onesSecondCounter.Enabled = true;
+
+                    tensSecondCounter.Enabled = true;
+
+                    onesMinuteCounter.Enabled = true;
+
+                    tensMinuteCounter.Enabled = true;
+                }
+                else if (onesSecondCounter.Value == -1)
+                {
+                    onesSecondCounter.Value = 9;
+                    tensSecondCounter.Value--;
+                }
+
+                // set tens second counter value
+                if (tensMinuteCounter.Value == 0 && onesMinuteCounter.Value == 0 && tensSecondCounter.Value == -1 && onesSecondCounter.Value == 0)
+                {
+                    tensSecondCounter.Value = 0;
+                }
+                else if (tensSecondCounter.Value == -1)
+                {
+                    tensSecondCounter.Value = 5;
+                    onesMinuteCounter.Value--;
+                }
+
+                // set ones minute counter value
+                if (tensMinuteCounter.Value == 0 && onesMinuteCounter.Value == -1 && tensSecondCounter.Value == 0 && onesSecondCounter.Value == 0)
+                {
+                    onesMinuteCounter.Value = 0;
+                }
+                else if (onesMinuteCounter.Value == -1)
+                {
+                    onesMinuteCounter.Value = 9;
+                    tensMinuteCounter.Value--;
+                }
+
+                // set tens minute counter value
+                if (tensMinuteCounter.Value == -1)
+                {
+                    tensMinuteCounter.Value = 0;
+                }
+            }));
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {   
+            if (controlButton.Text == "Start")
+            {
+                if (tensMinuteCounter.Value == -1 && onesMinuteCounter.Value == -1 && tensSecondCounter.Value == -1 && onesSecondCounter.Value == -1)
+                {
+                    tensMinuteCounter.Value = 0;
+                    onesMinuteCounter.Value = 0;
+                    tensSecondCounter.Value = 0;
+                    onesSecondCounter.Value = 0;
+                }
+                else
+                {
+                    controlButton.Text = "Stop";
+
+                    onesSecondCounter.Enabled = false;
+
+                    tensSecondCounter.Enabled = false;
+
+                    onesMinuteCounter.Enabled = false;
+
+                    tensMinuteCounter.Enabled = false;
+
+                    time.Start();
+                }
+            }
+            else
+            {
+                time.Stop();
+                controlButton.Text = "Start";
+
+                onesSecondCounter.Enabled = true;
+
+                tensSecondCounter.Enabled = true;
+
+                onesMinuteCounter.Enabled = true;
+
+                tensMinuteCounter.Enabled = true;
+            }
+
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            time.Stop();
+            Application.DoEvents();
+        }
+    }
+}
